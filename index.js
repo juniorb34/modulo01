3a9bb1e25ad848164efab3b48df9c8f1efe7d3ab
juniorb34 <juniorb34fortal@gmail.com>
2019-10-12 22:22:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const users = ["João J", "Junior", "Bosco"];
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+  return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { name } = req.body;
+  const { index } = req.params;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
+});
+
+server.listen(3000);
